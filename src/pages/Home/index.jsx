43210efe63659419efb8cfa4355628f77d5bbb8e
AndrewSantos24 <andrewdossantos34@gmail.com>
2024-09-12 00:12:02 -0300

    atualizando-repositorio

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,31 @@
+import './style.css'
+
+function Home() {
+
+  return (
+
+      <div className='container'>
+        <form>
+          <h1>Cadastro de Usuários</h1>
+          <input name='nome' type='text'/>
+          <input name='idade' type='number'/>
+          <input name='email' type='email'/>
+          <button type='button'>Cadastrar</button>
+        </form>
+
+        <div>
+          <div>
+            <p>Nome:</p>
+            <p>Idade:</p>
+            <p>Email:</p>
+          </div>
+          <button>
+            <img></img>
+          </button>
+        </div>
+
+      </div>
+  )
+}
+
+export default Home
