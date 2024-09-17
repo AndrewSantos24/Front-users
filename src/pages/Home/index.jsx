ba4b0ff33e152064f71ec6685a9c278fd1436ceb
AndrewSantos24 <andrewdossantos34@gmail.com>
2024-09-16 23:30:04 -0300

    fazendo-conexao-com-banco

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,46 @@
 import './style.css'
+import { useEffect,useState } from 'react'
+import Trash from '../../assets/trash.svg'
+import api from '../../services/api'
+
+//useState=hooks estado e uma variavel que muda as coisas na tela 
 
 function Home() {
+
+  const [users,setUsers] = useState([])
+
+  async function getUsers(){
+    const usersFromapi = await api.get('/usuarios')
+    setUsers(usersFromapi.data)
+    
+  }
+useEffect(()=>{
+  getUsers()
+},[])
 
   return (
 
       <div className='container'>
         <form>
           <h1>Cadastro de Usuários</h1>
-          <input name='nome' type='text'/>
-          <input name='idade' type='number'/>
-          <input name='email' type='email'/>
+          <input placeholder='Nome' name='nome' type='text'/>
+          <input placeholder='Idade' name='idade' type='number'/>
+          <input placeholder='email' name='email' type='email'/>
           <button type='button'>Cadastrar</button>
         </form>
 
-        <div>
-          <div>
-            <p>Nome:</p>
-            <p>Idade:</p>
-            <p>Email:</p>
-          </div>
-          <button>
-            <img></img>
-          </button>
-        </div>
-
+      {users.map(user=> (
+          <div key={user.id} className='card'>
+           <div>
+             <p>Nome: <span>{user.name}</span></p>
+             <p>Idade: <span>{user.age}</span></p>
+             <p>Email: <span>{user.email}</span></p>
+           </div>
+           <button>
+             <img src={Trash}/>
+           </button>
+         </div>
+      ))}
       </div>
   )
 }
